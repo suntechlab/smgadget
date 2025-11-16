af65279bad899a74d5dd60b813918c90bf32185e
Abdus Salam <salamsl2441@gmail.com>
2025-11-16 09:37:38 +0600

    fix

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,7 @@ import { CartContainder } from "@/components/Cart";
 export default function Cart() {
   return (
     <section className="py-12">
-      <CartContainder />
+      <CartContainder/>
     </section>
   );
 }
